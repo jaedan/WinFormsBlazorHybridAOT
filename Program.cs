9ead1ebd1bd14961944f5c1e36b655add9ceede6
Jaedan <jaedan@none.com>
2022-12-19 10:24:18 -0700

    Enable NativeAOT

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace WinFormsBlazorHybridAOT
 {
     internal static class Program
@@ -8,6 +10,8 @@ namespace WinFormsBlazorHybridAOT
         [STAThread]
         static void Main()
         {
+            ComWrappers.RegisterForMarshalling(WinFormsComInterop.WinFormsComWrappers.Instance);
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
